7d5133a6df54d5820b11e07fd74ac0d0681f32f8
Darrien Gunn <38056323+dgunn001@users.noreply.github.com>
2021-04-30 05:39:45 -0700

    Almost completed Level 1 Map, and other things
    -Added Backdrop
    -Added Boss room
    -Added collision with rest of walls and platforms
    -Added Player follow Camera

---
+++ News_Breakers_Game/Assets/Scripts/CameraController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform target;
+
+    public float smoothSpeed = 0.125f;
+    public Vector3 offset;
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 desiredPosition = target.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        transform.position = smoothedPosition; 
+    }
+}
