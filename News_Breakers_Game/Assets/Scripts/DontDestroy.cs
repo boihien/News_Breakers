9f885d12c52aa20dbeba5fe50b729c8541cb9aea
Darrien Gunn <dgunn001@ucr.edu>
2021-05-21 04:57:52 -0700

    Added Level Two Scene changes
    (Dont Destroy on Load)

---
+++ News_Breakers_Game/Assets/Scripts/DontDestroy.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontDestroy : MonoBehaviour
+{
+    private static bool created = false;
+
+    void Awake()
+    {
+        if (!created)
+        {
+            DontDestroyOnLoad(this.gameObject);
+            created = true;
+            Debug.Log("Awake: " + this.gameObject);
+        }
+    }
+}
